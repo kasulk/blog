031dcb6ab37c47eb5d0f6ce8c44b72bc695c7932
kasi <danielkaser@mailbox.org>
2024-04-10 16:57:04 +0200

    set up tailwind to apply general styles and to be able to add custom styles to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Hello from "@/content/blogs/hello.mdx";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Hello />
+      {/* prose-class neccessary for tailwind/typography to style the mdx */}
+      <div className="prose">
+        <Hello />
+      </div>
     </main>
   );
 }
