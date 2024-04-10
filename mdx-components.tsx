733444c6b5098e2625ac603ecc97f10437a0f3bc
kasi <danielkaser@mailbox.org>
2024-04-10 16:19:15 +0200

    prepare next.config for use of remark-gfm

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    /// Allows customizing built-in components, e.g. to add styling.
     ...components,
   };
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
-import nextMDX from "@next/mdx";
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
+import createMDX from "@next/mdx";
+// import remarkGfm from "remark-gfm"; /// for GitHub Flavored MD
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // remarkPlugins: [remarkGfm],
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 export default withMDX(nextConfig);
