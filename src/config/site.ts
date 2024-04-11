961ff8c3614ad28f744c37aa8623594acc194e9b
kasi <danielkaser@mailbox.org>
2024-04-11 16:47:34 +0200

    create config/site.ts for metadata and links

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "kasulk's blog",
+  url: "https://example.com",
+  description: "A really good blog.",
+  author: "kasulk",
+  links: {
+    personalSite: "https://danielkaser.de",
+    github: "https://github.com/kasulk",
+    codewars: "https://www.codewars.com/users/kasulk",
+
+    docs: {
+      arrayMap:
+        "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map?retiredLocale=de",
+    },
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
