2ed544545c51e23b916f5883d298ffc786b9bc64
kasi <danielkaser@mailbox.org>
2024-04-12 11:44:39 +0200

    create site header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import { SiteHeader } from "@/components/SiteHeader";
 
 if (process.env.NODE_ENV === "development") require("@/styles/devOnly.css");
 
@@ -22,10 +23,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          inter.variable
+          inter.variable,
         )}
       >
-        {children}
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader />
+          <main className="flex-1">{children}</main>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Hello from "@/content/blogs/hello.mdx";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* prose-class neccessary for tailwind/typography to style the mdx */}
+    <section className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* /// prose-class neccessary for tailwind/typography to style the mdx */}
       <div className="prose">
         <Hello />
       </div>
-    </main>
+    </section>
   );
 }
