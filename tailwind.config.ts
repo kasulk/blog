28e75e0e93f6f726708430c0ad28b6150a962873
kasi <danielkaser@mailbox.org>
2024-04-15 11:44:09 +0200

    fix tailwind config; mdx related settings got overwritten by shadcn install

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./mdx-components.tsx",
   ],
   prefix: "",
   theme: {
@@ -78,7 +79,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
