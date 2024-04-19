c294f74b1a62ac92b0089fdc9976bac61fb3c037
kasi <danielkaser@mailbox.org>
2024-04-19 14:45:48 +0200

    uninstall obsolete @next/mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import createMDX from "@next/mdx";
-// import remarkGfm from "remark-gfm"; /// for GitHub Flavored MD
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-};
+const nextConfig = {};
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    // remarkPlugins: [remarkGfm],
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
