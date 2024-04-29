a8cb016ed3c8da0681cb443a543f4ca657ce09f9
kasi <danielkaser@mailbox.org>
2024-04-29 13:05:31 +0200

    create formatBlogs(); implement into blog list on blogs page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { Metadata } from "next";
 import { getBlogs } from "./blogFetchers";
-import { formatDate } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
+import { formatDate } from "@/lib/utils";
+import { formatBlogs } from "./formatBlogs";
 
 export const metadata: Metadata = {
   title: `Mein Blog | ${siteConfig.name}`,
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 
 export default async function Blog() {
   const blogs = await getBlogs();
+  const formattedBlogs = formatBlogs(blogs);
 
   return (
     <main className="container prose mx-auto max-w-3xl py-6 dark:prose-invert">
@@ -19,31 +21,28 @@ export default async function Blog() {
       <section className="py-10">
         <h2>Letzte Blogs</h2>
         <ul className="list-none py-2 pl-0">
-          {blogs.map((blog) => {
+          {formattedBlogs.map((blog) => {
             const { frontmatter, slug } = blog;
-            const { title, description, author, pubDate, isPublished } =
-              frontmatter;
+            const { title, description, author, pubDate } = frontmatter;
 
             return (
-              isPublished && (
-                <Link
-                  className="no-underline"
-                  href={`/blog/${slug}`}
-                  key={slug}
-                  passHref
-                >
-                  <li className="my-5 flex list-none justify-between rounded-lg px-6 py-2 align-middle transition-colors duration-300 hover:bg-danger/20">
-                    <div>
-                      <h3 className="my-1">{title}</h3>
-                      <p className="my-1 text-gray-400">{description}</p>
-                    </div>
-                    <div className="my-auto flex flex-col items-end text-gray-400">
-                      <p className="my-1">{formatDate(pubDate, "de-DE")}</p>
-                      <p className="my-1 text-gray-400">{author}</p>
-                    </div>
-                  </li>
-                </Link>
-              )
+              <Link
+                className="no-underline"
+                href={`/blog/${slug}`}
+                key={slug}
+                passHref
+              >
+                <li className="my-5 flex list-none justify-between rounded-lg px-6 py-2 align-middle transition-colors duration-300 hover:bg-danger/20">
+                  <div>
+                    <h3 className="my-1">{title}</h3>
+                    <p className="my-1 text-gray-400">{description}</p>
+                  </div>
+                  <div className="my-auto flex flex-col items-end text-gray-400">
+                    <p className="my-1">{formatDate(pubDate, "de-DE")}</p>
+                    <p className="my-1 text-gray-400">{author}</p>
+                  </div>
+                </li>
+              </Link>
             );
           })}
         </ul>
