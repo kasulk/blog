dbbab2096b6a5491020aced38680703f27697499
kasi <danielkaser@mailbox.org>
2024-05-06 14:43:09 +0200

    create sortObjectKeys()

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,4 +5,5 @@ export * from "./createImageCreditsTag";
 export * from "./formatDate";
 export * from "./getInitials";
 export * from "./slugify";
+export * from "./sortObjectKeys";
 export * from "./spacifyCamelCase";
