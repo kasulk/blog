bfaff52f287689098155d3edac694d20c6573fa7
kasi <danielkaser@mailbox.org>
2024-05-31 13:19:29 +0200

    add hr above sidebar on small screens

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,11 +4,14 @@ type Props = {
 
 export function Sidebar({ children }: Props) {
   return (
-    <aside
-      id="sidebar"
-      className={`flex flex-wrap justify-evenly space-x-2 p-6 sm:gap-4 md:min-w-48 md:max-w-60 md:flex-col md:gap-12 md:p-0 lg:min-w-60`}
-    >
-      {children}
-    </aside>
+    <>
+      <hr className="m-0 flex w-full md:hidden" />
+      <aside
+        id="sidebar"
+        className={`flex flex-wrap justify-evenly space-x-2 p-6 sm:gap-4 md:min-w-48 md:max-w-60 md:flex-col md:gap-12 md:p-0 lg:min-w-60`}
+      >
+        {children}
+      </aside>
+    </>
   );
 }
