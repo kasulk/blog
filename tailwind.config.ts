d74c5ee1a12dd3c76c7aef5f832bee821171bb0c
kasi <danielkaser@mailbox.org>
2024-06-07 17:11:04 +0200

    remove bullet points for task lists compiled from markdown

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -149,6 +149,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      listStyleType: {
+        "contains-task-list": "none",
+      },
     },
   },
   plugins: [
