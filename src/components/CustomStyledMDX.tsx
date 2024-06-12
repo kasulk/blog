3b777ed2634414d2a4a6de2a8584c8b7826f55dd
kasi <danielkaser@mailbox.org>
2024-06-12 16:20:10 +0200

    refactor headings to feature auto-copy-to-clipboard; move auto-id from CustomStyledMDX to Headings.tsx

--- src/components/CustomStyledMDX.tsx
+++ src/components/CustomStyledMDX.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import { Link, ExternalLink, AffiliateLink } from "@/components/Links";
-import { SupportButton } from "@/components";
+import { H2, H3, H4, H5, H6, SupportButton } from "@/components";
 import { siteConfig } from "@/config";
 import * as links from "@/config/links";
-import { createId } from "@/lib/utils";
 
 export const customComponents: MDXComponents = {
   /// nextjs components
@@ -14,11 +13,11 @@ export const customComponents: MDXComponents = {
   /// HTML-elements compiled from MD(X)
   //! only works for compiled MD(X)-elements
   // e.g. '## Some Title' ==> '<h2>Some Title</h2>'
-  h2: ({ children }) => <h2 id={createId(children)}>{children}</h2>,
-  h3: ({ children }) => <h3 id={createId(children)}>{children}</h3>,
-  h4: ({ children }) => <h4 id={createId(children)}>{children}</h4>,
-  h5: ({ children }) => <h5 id={createId(children)}>{children}</h5>,
-  h6: ({ children }) => <h6 id={createId(children)}>{children}</h6>,
+  h2: (props) => <H2 {...props}>{props.children}</H2>,
+  h3: (props) => <H3 {...props}>{props.children}</H3>,
+  h4: (props) => <H4 {...props}>{props.children}</H4>,
+  h5: (props) => <H5 {...props}>{props.children}</H5>,
+  h6: (props) => <H6 {...props}>{props.children}</H6>,
   a: (props) => {
     const { href, title, children, ...restProps } = props;
     if (href?.startsWith("#")) return <a {...props}>{children}</a>;
--- src/components/Headings.tsx
+++ src/components/Headings.tsx
@@ -1,36 +1,125 @@
+"use client";
+
+import { ElementType, useState } from "react";
+import { usePathname } from "next/navigation";
+import { createId } from "@/lib/utils";
+import { Link2Icon } from "@radix-ui/react-icons";
+
 type HeadingProps = {
+  id?: string;
   className?: string;
   children: React.ReactNode;
 };
 
-export function H1({ className, children }: HeadingProps) {
+type CopyableHeadingProps = HeadingProps & {
+  as: ElementType;
+};
+
+export function H1({ id, className = "", children }: HeadingProps) {
   return (
-    <h1 className={`text-3xl sm:text-4xl lg:text-5xl ${className}`}>
+    <h1 id={id} className={`text-3xl sm:text-4xl lg:text-5xl ${className}`}>
       {children}
     </h1>
   );
 }
 
-export function H2({ className, children }: HeadingProps) {
+export function H2({ id, className = "", children }: HeadingProps) {
   return (
-    <h2 className={`mb-6 mt-0 text-2xl sm:text-3xl lg:text-4xl ${className}`}>
+    <CopyableHeading
+      as="h2"
+      id={id}
+      className={`mb-6 mt-0 text-2xl sm:text-3xl lg:text-4xl ${className}`}
+    >
       {children}
-    </h2>
+    </CopyableHeading>
   );
 }
 
-export function H3({ className, children }: HeadingProps) {
+export function H3({ id, className = "", children }: HeadingProps) {
   return (
-    <h3 className={`text-xl sm:text-2xl lg:text-3xl ${className}`}>
+    <CopyableHeading
+      as="h3"
+      id={id}
+      className={`text-xl sm:text-2xl lg:text-3xl ${className}`}
+    >
       {children}
-    </h3>
+    </CopyableHeading>
   );
 }
 
-export function H4({ className, children }: HeadingProps) {
+export function H4({ id, className = "", children }: HeadingProps) {
   return (
-    <h4 className={`text-lg sm:text-xl lg:text-2xl ${className}`}>
+    <CopyableHeading
+      as="h4"
+      id={id}
+      className={`text-lg sm:text-xl lg:text-2xl ${className}`}
+    >
       {children}
-    </h4>
+    </CopyableHeading>
+  );
+}
+
+export function H5({ id, className = "", children }: HeadingProps) {
+  return (
+    <CopyableHeading
+      as="h5"
+      id={id}
+      className={`text-base sm:text-lg lg:text-xl ${className}`}
+    >
+      {children}
+    </CopyableHeading>
+  );
+}
+
+export function H6({ id, className = "", children }: HeadingProps) {
+  return (
+    <CopyableHeading
+      as="h6"
+      id={id}
+      className={`text-sm sm:text-base lg:text-lg ${className}`}
+    >
+      {children}
+    </CopyableHeading>
+  );
+}
+
+function CopyableHeading({
+  id,
+  className,
+  children,
+  as: Tag,
+}: CopyableHeadingProps) {
+  const [tooltip, setTooltip] = useState("");
+  const pathname = usePathname();
+  id = id || createId(children);
+
+  function handleCopy() {
+    const link = `${window.location.origin}${pathname}#${id}`;
+    navigator.clipboard.writeText(link).then(() => {
+      setTooltip("Link kopiert!");
+      setTimeout(() => setTooltip(""), 1000);
+    });
+  }
+
+  return (
+    <div
+      className="group cursor-pointer"
+      onClick={handleCopy}
+      title="Link kopieren"
+    >
+      <Tag id={id} className={`flex items-center space-x-[0.5em] ${className}`}>
+        <span>{children}</span>
+        <Link2Icon className="h-[1em] w-[1em] opacity-0 transition-opacity duration-500 hover:text-accent group-hover:opacity-100" />
+        <Tooltip tooltip={tooltip} />
+      </Tag>
+    </div>
+  );
+}
+
+function Tooltip({ tooltip }: { tooltip: string }) {
+  return (
+    tooltip && (
+      <span className="whitespace-nowrap text-sm text-accent">{tooltip}</span>
+    )
   );
 }
