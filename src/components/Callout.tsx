fbfc8f86225bb6aa39dd5349fb6a5cfceb797a78
kasi <danielkaser@mailbox.org>
2024-06-17 15:52:22 +0200

    change Callout element from div to blockquote

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,31 +1,40 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
-interface CalloutProps {
-  type?: "default" | "warning" | "danger" | "success";
+export type CalloutType = "default" | "warning" | "danger" | "success";
+
+type Props = {
+  type?: CalloutType;
+  title?: string;
   className?: string;
   children?: ReactNode;
-}
+};
 
 export function Callout({
   type = "default",
+  title,
   className,
   children,
   ...props
-}: CalloutProps) {
+}: Props) {
+  const bgColor = `bg-${type}-100`;
+  const borderColor = `border-${type}-600`;
+  const titleColor = type === "default" ? "" : `text-${type}-600`;
+
   return (
-    <div
+    <blockquote
       className={cn(
-        `boder-l-4 my-6 w-full items-start rounded-md border p-4 dark:max-w-none ${className}`,
-        {
-          "border-danger-600 bg-danger-100 dark:prose": type === "danger",
-          "border-warning-600 bg-warning-100 dark:prose": type === "warning",
-          "border-success-600 bg-success-100 dark:prose": type === "success",
-        },
+        `my-6 items-start rounded-md border border-l-4 p-4 ${className}`,
+        `prose ${borderColor} ${bgColor}`,
       )}
       {...props}
     >
+      {title && (
+        <div className={`font-semibold ${titleColor}`}>
+          {title.toUpperCase()}
+        </div>
+      )}
       <div>{children}</div>
-    </div>
+    </blockquote>
   );
 }
