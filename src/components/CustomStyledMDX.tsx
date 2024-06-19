1c731c5168bb45462cb253e7cc71accc4e167a1e
kasi <danielkaser@mailbox.org>
2024-06-19 15:48:57 +0200

    map Callout conditionally to blockquote in CustomStyledMDX

--- src/components/CustomStyledMDX.tsx
+++ src/components/CustomStyledMDX.tsx
@@ -1,16 +1,22 @@
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
+import React from "react";
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import { Link, ExternalLink, AffiliateLink } from "@/components/Links";
 import { H2, H3, H4, H5, H6 } from "@/components/Headings";
-import { SupportButton } from "@/components";
-import { siteConfig } from "@/config";
+import { Callout, SupportButton } from "@/components";
+import { CalloutType, siteConfig } from "@/config";
 import * as links from "@/config/links";
 
 export const customComponents: MDXComponents = {
   /// nextjs components
   Image,
+  /// custom components
+  ExternalLink,
+  SupportButton: ({ className }) => (
+    <SupportButton className={`h-7 w-7 ${className}`} />
+  ),
   /// HTML-elements compiled from MD(X)
   //! only works for compiled MD(X)-elements
   // e.g. '## Some Title' ==> '<h2>Some Title</h2>'
@@ -31,10 +37,26 @@ export const customComponents: MDXComponents = {
       );
     return <ExternalLink {...props}>{children}</ExternalLink>;
   },
-  /// custom components
-  SupportButton: ({ className }) => (
-    <SupportButton className={`h-7 w-7 ${className}`} />
-  ),
+  blockquote: (props) => {
+    const children = React.Children.toArray(props.children);
+    const content = (children[1] as React.ReactElement)?.props
+      .children as string;
+    const typeTag = content.match(/\[\!(.*?)\]/); // e.g. extract 'note' from [!note]
+
+    if (!content || !typeTag) return <blockquote>{props.children}</blockquote>;
+
+    const [type, title] = typeTag[1].toLowerCase().split(":") as [
+      CalloutType,
+      string?,
+    ];
+    const text = content.split("\r\n").slice(1).join("\r\n");
+
+    return (
+      <Callout title={title || type} type={type}>
+        {text}
+      </Callout>
+    );
+  },
 };
 
 export function CustomStyledMDX(props: MDXRemoteProps) {
