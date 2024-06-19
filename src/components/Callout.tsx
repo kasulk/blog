94644f96466e02ef8c4d4c742902a4f10ea5e145
kasi <danielkaser@mailbox.org>
2024-06-19 15:05:46 +0200

    move calloutColorMap to site.ts

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,3 +1,5 @@
+import type { CalloutType } from "@/config";
+import { calloutColorMap as colorMap } from "@/config";
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
@@ -7,54 +9,6 @@ type Props = {
   className?: string;
   children?: ReactNode;
 };
-
-type ColorMap = typeof colorMap;
-export type CalloutType = keyof ColorMap;
-
-export const colorMap = {
-  default: {
-    titleColor: "text-muted-foreground",
-    textColor: "text-muted-foreground",
-    bgColor: "text-muted-foreground",
-    borderColor: "text-muted-foreground",
-  },
-  danger: {
-    titleColor: "text-danger-600",
-    textColor: "text-danger-900",
-    bgColor: "bg-danger-100",
-    borderColor: "border-danger-600",
-  },
-  warning: {
-    titleColor: "text-warning-600",
-    textColor: "text-warning-900",
-    bgColor: "bg-warning-100",
-    borderColor: "border-warning-600",
-  },
-  success: {
-    titleColor: "text-success-600",
-    textColor: "text-success-900",
-    bgColor: "bg-success-100",
-    borderColor: "border-success-600",
-  },
-  tip: {
-    titleColor: "text-success-600",
-    textColor: "text-success-900",
-    bgColor: "bg-success-100",
-    borderColor: "border-success-600",
-  },
-  important: {
-    titleColor: "text-fuchsia-600",
-    textColor: "text-fuchsia-900",
-    bgColor: "bg-fuchsia-100",
-    borderColor: "border-fuchsia-600",
-  },
-  note: {
-    titleColor: "text-blue-600",
-    textColor: "text-blue-900",
-    bgColor: "bg-blue-100",
-    borderColor: "border-blue-600",
-  },
-} as const;
 
 export function Callout({
   type = "default",
@@ -70,7 +24,7 @@ export function Callout({
   return (
     <blockquote
       className={cn(
-        `my-6 items-start rounded-md border border-l-4 p-4 text-foreground ${className}`,
+        `my-6 items-start rounded-md border border-l-4 p-4 ${className}`,
         // "prose",
         textColor,
         bgColor,
