03bdaa30a61491fa54a05ba1d6e07b552f7beaf6
kasi <danielkaser@mailbox.org>
2024-06-24 15:05:06 +0200

    refactor getAnchorFromLinkText()

--- src/lib/utils/getAnchorFromLinkText.ts
+++ src/lib/utils/getAnchorFromLinkText.ts
@@ -5,33 +5,29 @@ import { slugify } from "./slugify";
  * @function getAnchorFromLinkText
  * @description Converts React node children into an anchor link.
  * @param {React.ReactNode} children - The React node children from which to derive the anchor link.
- * @returns {string} A string representing the anchor link. If the content is a string, it returns a slugified version prefixed with '#'. Otherwise, it returns '/oops'.
- *
- * Example usage:
- * ```
- * const anchor = getAnchorFromLinkText(<div>Some Link Text</div>);
- * console.log(anchor); // Output: "#some-link-text" or "/oops" if the content is not a string
- * ```
- *
- * The function works as follows:
- * 1. Converts the `children` prop to an array using `React.Children.toArray`.
- * 2. Extracts the first element and assigns it to `rawContent`.
- * 3. Attempts to traverse up to three levels deep into nested children, looking for a string type.
- * 4. If a string is found, it is slugified and prefixed with '#'. Otherwise, returns "/oops".
+ * @param {number} maxDepth - Maximum depth to search for string content (default is 3).
+ * @returns {string} A string representing the anchor link. If the content is a string, it returns a slugified version prefixed with '#'. Throws an error if no string content is found within the specified depth.
+ * @throws Will throw an error if no string content is found within the specified depth.
  */
-export function getAnchorFromLinkText(children: React.ReactNode): string {
-  const childrenArr = React.Children.toArray(children);
-  let rawContent = childrenArr[0] as React.ReactElement;
-  console.log("rawContent (before):", rawContent);
+export function getAnchorFromLinkText(
+  children: React.ReactNode,
+  maxDepth: number = 3,
+): string {
+  let currNode = children;
+  let currDepth = 0;
 
-  // limited to 3 tests for nested children
-  for (let i = 0; i < 3 && typeof rawContent !== "string"; i++) {
-    const nextChildrenArr = React.Children.toArray(rawContent);
-    rawContent = (nextChildrenArr[0] as React.ReactElement).props.children;
+  while (currDepth < maxDepth) {
+    const childrenArr = React.Children.toArray(currNode);
+    if (!childrenArr.length) break;
+
+    currNode = childrenArr[0];
+
+    if (typeof currNode === "string") return "#" + slugify(currNode);
+    else if (!React.isValidElement(currNode)) break;
+
+    currNode = (currNode as React.ReactElement).props.children;
+    currDepth++;
   }
 
-  const anchor =
-    typeof rawContent === "string" ? "#" + slugify(rawContent) : "/oops";
-
-  return anchor;
+  throw new Error("No string content found within the specified depth.");
 }
