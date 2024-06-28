6aa0cddb4a7b8abd3f1aaf87e36a98b0ecbd1496
kasi <danielkaser@mailbox.org>
2024-06-28 15:13:40 +0200

    adjust SiteFooter, SiteHeader and SocialLinks to be Codewars-icon-ready

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,4 +1,5 @@
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Codewars as CodewarsLogoIcon } from "./Icons";
 import { SupportButton } from "@/components/SupportButton"; //? barrel imports not working here (server/client-conflict?)
 import { ExternalLink } from "@/components/Links";
 import { Button } from "./ui/button";
@@ -13,34 +14,35 @@ const socialIcons = [
   // same order as in social links obj
   <GitHubLogoIcon key="github" className={socialIconSize} />,
   <LinkedInLogoIcon key="linkedin" className={socialIconSize} />,
-  // <CodewarsLogoIcon key="codewars" className={socialIconSize} />,
+  <CodewarsLogoIcon key="codewars" className={socialIconSize} />,
 ];
 
 type Props = {
   className?: string;
   showEmail?: boolean;
+  showCodewars?: boolean;
   emailSubject?: string;
 };
 
 export function SocialLinks({
   className,
   showEmail = true,
+  showCodewars = true,
   emailSubject,
 }: Props) {
   // hide link if there is no icon for it
   const linksWithIcon = links.social.filter((_, i) => i < socialIcons.length);
+  const linksToShow = !showCodewars
+    ? linksWithIcon.filter(({ name }) => name !== "Codewars")
+    : linksWithIcon;
 
   return (
-    <>
+    <div className={className}>
       {showEmail && (
         <EmailButton subject={emailSubject} className={socialIconSize} />
       )}
-      {linksWithIcon.map(({ name, url }, i) => (
-        <ExternalLink
-          key={`${slugify(name)}-link`}
-          href={url}
-          className={className}
-        >
+      {linksToShow.map(({ name, url }, i) => (
+        <ExternalLink key={`${slugify(name)}-link`} href={url}>
           <Button className="w-10 px-0 text-foreground" variant="ghost">
             {socialIcons[i]}
             <span className="sr-only">{`Link to ${name}`}</span>
@@ -48,6 +50,6 @@ export function SocialLinks({
         </ExternalLink>
       ))}
       <SupportButton className={socialIconSize} />
-    </>
+    </div>
   );
 }
