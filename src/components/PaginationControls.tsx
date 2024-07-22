b0d739696cca32be5f798fcd8b1b294b96c53aca
kasi <danielkaser@mailbox.org>
2024-07-22 14:51:21 +0200

    add radix icons to pagination controls

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -2,6 +2,12 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { CategoryBadge } from "./CategoryBadge";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  PinLeftIcon,
+  PinRightIcon,
+} from "@radix-ui/react-icons";
 
 type Props = {
   numBlogPosts: number;
@@ -30,56 +36,54 @@ export function PaginationControls({
   }
 
   return (
-    <>
+    <div className="flex items-center justify-center">
       {numBlogPosts > 10 && (
-        <div className="flex items-center justify-center">
-          <div className="flex h-0 w-full flex-wrap justify-center gap-[2%] sm:gap-4">
-            <div className={`${boxClass} order-1 sm:order-1 `}>
-              <CategoryBadge
-                className={buttonClass}
-                disabled={!hasPrevPage}
-                onClick={() => router.push(getNewSearchParams(1))}
-              >
-                &lt;&lt;
-              </CategoryBadge>
-            </div>
-            <div className={`${boxClass} order-2 sm:order-2`}>
-              <CategoryBadge
-                className={buttonClass}
-                disabled={!hasPrevPage}
-                onClick={() => router.push(getNewSearchParams(page - 1))}
-              >
-                &lt;
-              </CategoryBadge>
-            </div>
-            <div
-              className={`${boxClass} order-5 mt-1 w-full items-center justify-center px-4 sm:order-3 sm:mt-auto sm:w-auto sm:max-w-sm`}
+        <div className="flex h-0 w-full flex-wrap justify-center gap-[2%] sm:gap-4">
+          <div className={`${boxClass} order-1 sm:order-1 `}>
+            <CategoryBadge
+              className={buttonClass}
+              disabled={!hasPrevPage}
+              onClick={() => router.push(getNewSearchParams(1))}
             >
-              <div className="p-1 px-4">
-                {page} / {numPages}
-              </div>
+              <PinLeftIcon />
+            </CategoryBadge>
+          </div>
+          <div className={`${boxClass} order-2 sm:order-2`}>
+            <CategoryBadge
+              className={buttonClass}
+              disabled={!hasPrevPage}
+              onClick={() => router.push(getNewSearchParams(page - 1))}
+            >
+              <ChevronLeftIcon />
+            </CategoryBadge>
+          </div>
+          <div
+            className={`${boxClass} order-5 mt-1 w-full items-center justify-center px-4 sm:order-3 sm:mt-auto sm:w-auto sm:max-w-sm`}
+          >
+            <div className="p-1 px-4">
+              {page} / {numPages}
             </div>
-            <div className={`${boxClass} order-3 sm:order-4`}>
-              <CategoryBadge
-                className={buttonClass}
-                disabled={!hasNextPage}
-                onClick={() => router.push(getNewSearchParams(page + 1))}
-              >
-                &gt;
-              </CategoryBadge>
-            </div>
-            <div className={`${boxClass} order-4 sm:order-5`}>
-              <CategoryBadge
-                className={buttonClass}
-                disabled={!hasNextPage}
-                onClick={() => router.push(getNewSearchParams(numPages))}
-              >
-                &gt;&gt;
-              </CategoryBadge>
-            </div>
+          </div>
+          <div className={`${boxClass} order-3 sm:order-4`}>
+            <CategoryBadge
+              className={buttonClass}
+              disabled={!hasNextPage}
+              onClick={() => router.push(getNewSearchParams(page + 1))}
+            >
+              <ChevronRightIcon />
+            </CategoryBadge>
+          </div>
+          <div className={`${boxClass} order-4 sm:order-5`}>
+            <CategoryBadge
+              className={buttonClass}
+              disabled={!hasNextPage}
+              onClick={() => router.push(getNewSearchParams(numPages))}
+            >
+              <PinRightIcon />
+            </CategoryBadge>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
