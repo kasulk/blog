d5ea0f8d252120e7928202ae01f73d1ef3a2f378
kasi <danielkaser@mailbox.org>
2024-08-01 18:00:53 +0200

    fix borders, border radius and colors on Laptop component

--- src/components/PortfolioPage/Laptop/Laptop.tsx
+++ src/components/PortfolioPage/Laptop/Laptop.tsx
@@ -8,16 +8,15 @@ type Props = {
 const laptopColor1 = "bg-[#3a3d40]";
 const laptopColor2 = "bg-[#18171966]";
 
-const laptopShadowColor1 = laptopColor2;
-const laptopShadowColor2 = "#18171999";
-const laptopShadowColor3 = "#3a3d4011";
+const laptopShadowColor = laptopColor2;
+const borders = `border-outset border border-gray-900/60 border-b-gray-800 border-r-gray-800`;
 
 export function Laptop({ imageSrc, imageAlt }: Props) {
   return (
     <div className="absolute left-0 top-0 flex h-[90%] w-full flex-col items-center">
       {/* laptop top */}
       <div
-        className={`flex h-full w-4/5 items-center justify-center rounded-[2%] ${laptopColor1}`}
+        className={`flex h-full w-4/5 items-center justify-center rounded-[2%] ${laptopColor1} ${borders}`}
       >
         {/* laptop-cam */}
         <div
@@ -40,16 +39,22 @@ export function Laptop({ imageSrc, imageAlt }: Props) {
         </div>
       </div>
       {/* laptop-bottom */}
-      <div className={`absolute bottom-0 flex h-[8%] w-full ${laptopColor1}`}>
+      <div className={`absolute bottom-0 flex h-[8%] w-full`}>
         {/* laptop-bottom-left */}
-        <div className="h-full w-[10%] border-r-0"></div>
+        <div
+          className={`${borders} h-full w-[10%] rounded-[20%_0_0_50%] border-r-0 ${laptopColor1}`}
+        ></div>
         {/* laptop-bottom-middle */}
-        <div className="flex h-full w-4/5 justify-center border-l-0 border-r-0">
+        <div
+          className={`${borders} flex h-full w-4/5 justify-center border-l-0 border-r-0 ${laptopColor1}`}
+        >
           {/* laptop-lock */}
-          <div className={`h-2/5 w-1/5 ${laptopColor2}`}></div>
+          <div className={`h-2/5 w-1/5 ${laptopColor2} ${borders}`}></div>
         </div>
         {/* laptop-bottom-right */}
-        <div className="h-full w-[10%] border-l-0"></div>
+        <div
+          className={`${borders} h-full w-[10%] rounded-[0_20%_50%_0] border-l-0 ${laptopColor1}`}
+        ></div>
       </div>
     </div>
   );
@@ -57,37 +62,37 @@ export function Laptop({ imageSrc, imageAlt }: Props) {
 
 //todo: add missing (custom) classes
 
-// .laptop-top {
-//   box-shadow: 5px 0px 8px -4px var(--laptop-shadow-color);
-// }
+/// .laptop-top {
+///   box-shadow: 5px 0px 8px -4px var(--laptop-shadow-color);
+/// }
 
-// .laptop-lock {
-//   border: 1px outset var(--laptop-border-color1);
-//   border-right: 1px outset var(--laptop-border-color2);
-//   border-bottom: 1px outset var(--laptop-border-color2);
-// }
+///-- .laptop-lock {
+///--   border: 1px outset var(--laptop-border-color1);
+///--  border-right: 1px outset var(--laptop-border-color2);
+///--   border-bottom: 1px outset var(--laptop-border-color2);
+///-- }
 
-// .laptop-top,
-// .laptop-bottom-left,
-// .laptop-bottom-middle,
-// .laptop-bottom-right {
-//   border: 1px outset var(--laptop-border-color1);
-//   border-right: 1px outset var(--laptop-border-color2);
-//   border-bottom: 1px outset var(--laptop-border-color2);
-// }
+///-- .laptop-top,
+///-- .laptop-bottom-left,
+///-- .laptop-bottom-middle,
+///-- .laptop-bottom-right {
+///--   border: 1px outset var(--laptop-border-color1);
+///--   border-right: 1px outset var(--laptop-border-color2);
+///--   border-bottom: 1px outset var(--laptop-border-color2);
+///-- }
 
-// .screen-effects {
-//   background: linear-gradient(33deg, transparent 60%, var(--main-white) 40%);
-// }
+/// .screen-effects {
+///   background: linear-gradient(33deg, transparent 60%, var(--main-white) 40%);
+/// }
 
-// .laptop-bottom {
-//   box-shadow: 0 5px 5px -5px var(--laptop-shadow-color);
-// }
+/// .laptop-bottom {
+///   box-shadow: 0 5px 5px -5px var(--laptop-shadow-color);
+/// }
 
-// .laptop-bottom-left {
-//   border-radius: 20% 0 0 50%;
-// }
+///-- .laptop-bottom-left {
+///--   border-radius: 20% 0 0 50%;
+///-- }
 
-// .laptop-bottom-right {
-//   border-radius: 0 20% 50% 0;
-// }
+///-- .laptop-bottom-right {
+///--   border-radius: 0 20% 50% 0;
+///-- }
