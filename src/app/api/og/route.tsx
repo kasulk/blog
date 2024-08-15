6d0f7e1042afddb7cdac9a3cff044034789c5793
kasi <danielkaser@mailbox.org>
2024-08-15 16:05:15 +0200

    add semantic html to dynamic og images; add robots.txt

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,8 +5,9 @@ import * as links from "@/config/links";
 import { fetchCodewarsChallengeAPI } from "@/lib/apiFetchers";
 
 // docs: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation
+// created on: https://og-playground.vercel.app
 
-export const runtime = "edge"; // for hosting on Vercel
+export const runtime = "edge"; // if hosted on Vercel
 
 const interBold = fetch(
   new URL("../../../assets/fonts/Inter-Bold.ttf", import.meta.url),
@@ -14,30 +15,31 @@ const interBold = fetch(
 
 export async function GET(req: NextRequest) {
   try {
-    const fontBold = await interBold;
+    const fontData = await interBold;
     const { searchParams } = req.nextUrl;
+
+    // title
     const title = searchParams.get("title");
-
-    // level
-    const id = searchParams.get("codewars");
-    const codeChallengeData = id ? await fetchCodewarsChallengeAPI(id) : null;
-    const level = codeChallengeData?.level;
-
-    // description
-    const description = searchParams.get("desc");
-
-    //Todo:
-    //language
-    //read time
-
     if (!title) return new Response("No title provided", { status: 500 });
 
     const heading = title.split("|");
     const isMultiLineHeading = heading.length > 1;
 
+    // description
+    const description = searchParams.get("desc");
+
+    // level
+    const isCodeChallenge = searchParams.has("codewars");
+    const id = searchParams.get("codewars");
+    const codeChallengeData = id ? await fetchCodewarsChallengeAPI(id) : null;
+    const level = codeChallengeData?.level;
+
+    //Todo:
+    //language
+    //read time
+
     return new ImageResponse(
       (
-        // created on: https://og-playground.vercel.app
         <div
           tw="flex relative flex-col p-12 pb-4 w-full h-full items-start text-white/90"
           style={{
@@ -46,7 +48,7 @@ export async function GET(req: NextRequest) {
           }}
         >
           {/* branding */}
-          <div tw="flex items-center">
+          <h1 tw="flex p-0 m-0 mb-2 items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -60,56 +62,58 @@ export async function GET(req: NextRequest) {
               <path d="M4 4a16 16 0 0 1 16 16" />
               <circle cx="5" cy="19" r="1" />
             </svg>
-            <p tw="ml-2 font-bold text-2xl">{branding}</p>
-          </div>
-          {/* header */}
-          <div tw="flex flex-col py-10">
-            <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
+            <span tw="ml-2 text-2xl">{branding}</span>
+          </h1>
+
+          <header tw="flex flex-col my-10">
+            <h2 tw="flex m-0 mb-2 text-xl uppercase tracking-tight">
               BLOG POST
-            </div>
+            </h2>
             {heading.map((line, i) => (
-              <div
+              <h3
                 key={`title-part-${i}`}
-                tw={`flex text-[50px] ${!i && isMultiLineHeading && "text-white/65"}`}
+                tw={`flex m-0 text-6xl ${!i && isMultiLineHeading && "text-white/65"}`}
               >
                 {line}
-              </div>
+              </h3>
             ))}
-          </div>
-          {/* content */}
-          <div tw="flex flex-1 text-2xl p-10 pt-0 pb-5">
+          </header>
+
+          <main tw="flex flex-1 text-2xl p-10 pt-0 pb-5">
             <p>{description}</p>
-          </div>
-          {/* footer */}
-          <div tw="flex items-end w-full justify-between">
+          </main>
+
+          <footer tw="flex items-end w-full justify-between">
             <div tw="flex justify-end text-xl">{siteConfig.url}</div>
             <div tw="flex flex-col items-end text-xl">
               {links.social.map(({ url }) => {
                 const shortUrl = url.replace(/https?:\/\/|www\./gi, "");
                 return (
-                  <div key={shortUrl} tw="flex ml-2">
+                  <a href={url} key={shortUrl} tw="flex ml-2">
                     {shortUrl}
-                  </div>
+                  </a>
                 );
               })}
             </div>
-          </div>
+          </footer>
         </div>
       ),
       {
         width: 1200,
         height: 630,
+        emoji: "twemoji",
         fonts: [
           {
             name: "Inter",
-            data: fontBold,
+            data: fontData,
             style: "normal",
             weight: 700,
           },
         ],
       },
     );
-  } catch (error) {
+  } catch (error: any) {
+    console.log(`${error.message}`);
     return new Response("Failed to generate image", { status: 500 });
   }
 }
