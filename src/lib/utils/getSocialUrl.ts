d130b4f5378b92793289c9b5d3146eb960ec96d8
kasi <danielkaser@mailbox.org>
2024-08-23 13:51:49 +0200

    update getSocialUrl() to handle case insensitive input

--- src/lib/utils/getSocialUrl.ts
+++ src/lib/utils/getSocialUrl.ts
@@ -1,7 +1,9 @@
 import * as links from "@/config/links";
 
 export function getSocialUrl(socialName: string): string {
-  const social = links.social.find(({ name }) => socialName === name);
+  const social = links.social.find(
+    ({ name }) => socialName.toLowerCase() === name.toLowerCase(),
+  );
   if (!social) {
     throw new Error(
       `Uh-oh! Social (link) for '${socialName}' does not exist...`,
