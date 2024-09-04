d0962a2411f753a27f9788a882e01199ceb00fe5
kasi <danielkaser@mailbox.org>
2024-09-04 13:35:44 +0200

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@/components/Links";
-import { H2 } from "@/components/Headings";
+import { H2, H3, H4 } from "@/components/Headings";
 import { PageHeader } from "@/components";
 
 export default function Home() {
@@ -7,33 +7,31 @@ export default function Home() {
     <>
       <PageHeader>Salü</PageHeader>
 
-      <div className="text-center">
-        <H2>Hier findest du in Kürze</H2>
-        <ul className="list-none">
+      <div className="mb-8 text-center">
+        <H2>Hier findest Du</H2>
+        <ul className="list-none p-0">
           <li>
             meinen <Link href="/blog">Blog</Link>{" "}
           </li>
+          <li>und</li>
           <li>
             mein <Link href="/portfolio">Portfolio</Link>{" "}
           </li>
         </ul>
 
-        <H2>Bis es soweit ist, kannst du</H2>
-        <ul className="list-none">
+        <H3>Außerdem kannst Du hier</H3>
+        <ul className="list-none p-0">
           <li>
-            <Link href="/aboutme">hier </Link> mehr über mich erfahren
+            <Link href="/aboutme">mehr über mich erfahren</Link>
           </li>
           <li>oder</li>
           <li>
-            <Link href="/support">hier</Link> den Blog unterstützen.
+            <Link href="/support">den Blog unterstützen</Link>
           </li>
         </ul>
 
-        <H2>Danke!</H2>
-        <p>
-          Bis in Bälde, <br />
-          Daniel
-        </p>
+        <H4 className="mt-12">Bis in Bälde!</H4>
+        <Link href="/aboutme">Daniel</Link>
       </div>
     </>
   );
