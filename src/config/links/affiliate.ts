ea83264e9052d89cf5aeac91c401265d0ad424aa
kasi <danielkaser@mailbox.org>
2025-04-04 11:15:11 +0200

    add swaper affiliate data; update some affiliate descriptions

--- src/config/links/affiliate.ts
+++ src/config/links/affiliate.ts
@@ -16,7 +16,8 @@ export const affiliate = [
   },
   {
     name: "Bondora",
-    description: "",
+    description:
+      "Bondora ist eine estnische P2P-Plattform, die Konsumentenkredite in Europa anbietet. Sie ist bekannt für ihr Produkt Go & Grow, das automatische Investitionen mit bis zu 6,75 % Rendite ermöglicht. Einfach zu bedienen, mit täglicher Liquidität und ohne Mindestanlage.",
     url: "https://bondora.com/ref/danielk39",
     type: "register",
     category: "Finanzen",
@@ -26,12 +27,13 @@ export const affiliate = [
       user: "5 Euro",
     },
     payoutOn: "nach 90 Tagen",
-    lastUpdated: new Date("2025-03-24"),
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
     name: "Debitum",
-    description: "",
+    description:
+      "Debitum ist eine europäische P2P-Plattform, die in Geschäftskredite investiert, vergeben von geprüften Darlehensanbahnern. Sie bietet bis zu 11 % Rendite, kurze Laufzeiten und Rückkaufgarantien. Der Fokus liegt auf Finanzierungen von kleineren und mittleren Unternehmen mit transparenter Risikobewertung und regulatorischer Konformität.",
     url: "https://debitum.investments/en/r/PRU4I",
     type: "register",
     category: "Finanzen",
@@ -45,27 +47,29 @@ export const affiliate = [
       "<b>Beispiel:</b><br>" +
       '"Wenn Ihr Freund 5000 EUR investiert, erhalten Sie beide einen Bonus von 2,5% für 1000 EUR + 1% für den Restbetrag.<br>' +
       '(25 EUR + 1% von den verbleibenden 4000 EUR) = 65 EUR" &mdash; Debitum',
-    lastUpdated: new Date("2025-03-24"),
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
     name: "Flatex",
-    description: "",
-    url: "",
+    description:
+      "Flatex ist ein deutscher Online-Broker, der den Handel mit Aktien, ETFs, Fonds und Derivaten anbietet. Er richtet sich an Privatanleger, bietet eine benutzerfreundliche Plattform, pauschale Ordergebühren und Zugang zu deutschen sowie internationalen Börsen. Sitz: Frankfurt am Main.",
+    url: "https://konto.flatex.de/cci/?referrerID=5137ad0c-fc11-11ef-b328-0242ac110006",
     type: "register",
     category: "Finanzen",
     subCategory: "Broker",
     rewards: {
-      referrer: "",
-      user: "",
+      referrer: "50 Euro Orderguthaben",
+      user: "50 Euro Orderguthaben",
     },
-    payoutOn: "",
-    lastUpdated: new Date("2024-05-13"),
+    payoutOn: "Registrierung/Depot-Eröffnung des Geworbenen",
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
     name: "Interactive Brokers",
-    description: "",
+    description:
+      "Interactive Brokers ist ein weltweit führender Online-Broker für Aktien, ETFs, Optionen, Futures, Forex und mehr. Die Plattform richtet sich an aktive Trader und professionelle Investoren, bietet niedrige Gebühren, leistungsstarke Tools, weltweiten Marktzugang und eine robuste Handelsplattform.",
     url: "https://ibkr.com/referral/daniel471",
     type: "register",
     category: "Finanzen",
@@ -75,13 +79,13 @@ export const affiliate = [
       user: "",
     },
     payoutOn: "",
-    lastUpdated: new Date("2025-03-07"),
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
     name: "Lande",
     description:
-      "LANDE ist eine Crowdfunding-Plattform, die sich auf die Finanzierung des Agrarsektors konzentriert.",
+      "Lande ist eine europäische Crowdfunding-Plattform, die sich auf durch landwirtschaftliche Vermögenswerte besicherte Kredite spezialisiert. Investoren finanzieren Bauernprojekte und erhalten Zinsen von bis zu 12 %. Die Kredite sind meist durch Land, Ernten oder Maschinen abgesichert.",
     url: "https://lande.finance?referral=F7QGGIB",
     type: "register",
     category: "Finanzen",
@@ -92,7 +96,23 @@ export const affiliate = [
     },
     payoutOn:
       "Investments auf dem Primärmarkt in den ersten 90 Tagen nach der Registrierung des Geworbenen",
-    lastUpdated: new Date("2025-03-24"),
+    lastUpdated: new Date("2025-03-27"),
+    blog: "",
+  },
+  {
+    name: "Malt",
+    description:
+      "Malt ist eine europäische Freelancer-Plattform, die Unternehmen mit selbstständigen Experten in Bereichen wie IT, Design, Marketing und Consulting verbindet. Sie bietet transparente Projektabwicklung, sichere Bezahlung und persönliche Betreuung. Besonders stark im DACH- und französischen Markt.",
+    url: "",
+    type: "register",
+    category: "",
+    subCategory: "",
+    rewards: {
+      referrer: "",
+      user: "",
+    },
+    payoutOn: "",
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
@@ -114,7 +134,8 @@ export const affiliate = [
   },
   {
     name: "Robocash",
-    description: "",
+    description:
+      "Robocash ist eine automatisierte Peer-to-Peer-Investmentplattform, die in kurzfristige Verbraucherkredite investiert, vor allem in Asien und Europa. Sie bietet bis zu 13 % Rendite, Rückkaufgarantie und gehört zu einer profitablen Firmengruppe mit eigener Kreditvergabe. Ideal für passives Investieren.",
     url: "https://robo.cash/ref/ajTe",
     type: "register",
     category: "Finanzen",
@@ -125,22 +146,40 @@ export const affiliate = [
     },
     payoutOn:
       "Investitionen von mind. 500 Euro innerhalb der ersten 30 Tage nach Registrierung. Maximal 10 Euro.",
-    lastUpdated: new Date("2025-03-24"),
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
     name: "Swaper",
-    description: "",
-    url: "",
+    description:
+      "Swaper ist eine Peer-to-Peer-Plattform, die Investoren Zugang zu kurzfristigen Verbraucherkrediten bietet, hauptsächlich aus der Westeuropäischen Region. Sie lockt mit bis zu 14 % Rendite, automatisierten Investitionen und einer Rückkaufgarantie für ausgefallene Kredite.",
+    url: "https://www.swaper.com/#/ref/8CaT5W9hYr",
     type: "register",
     category: "Finanzen",
     subCategory: "P2P-Kredite",
+    rewards: {
+      referrer:
+        "2% des in den ersten 30 Tagen nach der Registrierung eingezahlten Betrages des Geworbenen",
+      user: "leider z.Zt. nüscht...",
+    },
+    payoutOn: "35 Tage nach der Einzahlung des Geworbenen",
+    lastUpdated: new Date("2025-03-27"),
+    blog: "",
+  },
+  {
+    name: "Tastytrade",
+    description:
+      "Tastytrade (ehemals Tastyworks) ist eine US-basierte Handelsplattform, spezialisiert auf Optionen, Futures und Aktien. Sie richtet sich an aktive Trader, bietet innovative Tools, niedrige Gebühren und edukative Inhalte. Der Fokus liegt auf strategiegetriebenem, selbstbestimmtem Trading mit hohem Lernfaktor.",
+    url: "",
+    type: "register",
+    category: "Finanzen",
+    subCategory: "Broker",
     rewards: {
       referrer: "",
       user: "",
     },
     payoutOn: "",
-    lastUpdated: new Date("2024-05-13"),
+    lastUpdated: new Date("2025-03-27"),
     blog: "",
   },
   {
